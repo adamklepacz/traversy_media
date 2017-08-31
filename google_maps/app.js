8ccf3d346fd56d80a338aa15c9a9bb6c5070de6b
Adam Klepacz <adamklepacz92@gmail.com>
2017-08-31 19:55:52 +0200

    update

--- google_maps/app.js
+++ google_maps/app.js
@@ -8,6 +8,10 @@ function initMap() {
 	//new map
 	var map = new google.maps.Map(document.getElementById('map'), options);
 	
+	google.maps.event.addListener(map, 'click', function(e) {
+		//add marker
+		addMarker({coords: e.latLng});
+	});
 	
 	/*
 	var markerPoint =  {lat: 51.1105849, lng:	17.0254199};
@@ -30,16 +34,32 @@ function initMap() {
 	*/
 	addMarker({
 		coords:	{lat: 51.1105849, lng:	17.0254199},
-		iconImage: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'
+		iconImage: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+		content: '<h1>Siedlikow</h1>'
 	});
 	addMarker({coords:	{lat: 51.4784294, lng:	17.9618529}});
+	addMarker({coords:	{lat: 51.4784294, lng:	17.9618549}});
 	//add marker from input
 	function addMarker(props) {
 		marker = new google.maps.Marker({
 			position: props.coords,
 			map: map,
-			icon:	props.iconImage
+			//icon:	props.iconImage
   	});
+		
+		if(props.iconImage) {
+			marker.setIcon(props.iconImage);
+		}
+		
+		var infoWindow = new google.maps.InfoWindow({
+			content: props.content
+		});
+	
+		marker.addListener('click', function() {
+			if(props.content) {
+				infoWindow.open(map, marker);
+			}
+		});  
 	}
 }
 
