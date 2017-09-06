c9b9ad34e6fe28180c0afdc14a0520f57b0fed3e
Adam Klepacz <adamklepacz92@gmail.com>
2017-09-06 12:56:00 +0200

    update

--- ajax_course/process.php
+++ ajax_course/process.php
@@ -1,4 +1,9 @@
 <?php 
+
+//check for POST variable
+if(isset($_POST['name'])) {
+	echo 'POST: Your name is:'. $_POST['name'];
+}
 //check for GET variable
 if(isset($_GET['name'])) {
 	echo 'GET: Your name is:'. $_GET['name'];
